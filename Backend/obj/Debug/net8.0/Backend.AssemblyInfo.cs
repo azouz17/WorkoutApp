15ebb578669a5f2b6f4a2d6ba3e95470e548fb8d
azouz17 <78298485+azouz17@users.noreply.github.com>
2024-12-22 14:43:48 +0200

    test push

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d739b37598c31a2f32e9c723a6ee24d97563af79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7db9130493e7fa8290932dec93949a53f5ac12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
